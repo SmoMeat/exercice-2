3ef0cc8e4428977c7784b55a033bbc72f084e229
Mathieu <mathieuducharme86@gmail.com>
2024-01-30 09:56:34 -0500

    final fix

--- parabola_solutions.py
+++ parabola_solutions.py
@@ -13,53 +13,29 @@ consultez 'https://www.alloprof.qc.ca/fr/eleves/bv/mathematiques/la-formule-quad
 
 from math import sqrt
 
-def get_solutions_of_parabola(a: int | float, b: int | float, c: int | float) -> [float]:
-    """
-    Cette fonction retourne une liste contenant toutes les solutions réelles d'une équation du second degré de la forme ax²+bx+c où a≠0.
-    Args:
-        a (int ou float): coefficient associé à x puissance 2
-        b (int ou float): coefficient associé à x puissance 1
-        c (int ou float): coefficient associé à x puissance 0
-    Returns:
-        liste de float: une liste contenant des floats représentant les solutions (si aucune solution réelle n'existe, la liste est vide)
-    """
+parabolas = [
+    {"a": 2, "b": 5, "c": 2},   # Premier polynome
+    {"a": 1, "b": 2, "c": 1},   # Deuxième polynome
+    {"a": 2, "b": 2, "c": 2}    # Troisième polynome
+]
 
-    discriminant = b**2 - 4 * a * c
-    solutions = []
+for parabola in parabolas:
+    a = parabola['a']   # coefficient associé à x puissance 2
+    b = parabola['b']   # coefficient associé à x puissance 1
+    c = parabola['c']   # coefficient associé à x puissance 0
 
-    if discriminant < 0:
-        return solutions
-    
-    solutions.append((-b + sqrt(discriminant)) / (2 * a))
+    discriminant = b**2 - 4 * a * c     # Discriminant (Δ) pour déterminer cmb de solutions existent
 
-    if discriminant == 0:
-        return solutions
-    
-    solutions.append((-b - sqrt(discriminant)) / (2 * a))
+    if discriminant < 0: # Cas avec 0 solution
+        print(f"Il n'existe aucune solution à l'équation {a}x²+{b}x+{c}.")
+        continue
 
-    return solutions
+    first_solution = (-b + sqrt(discriminant)) / (2 * a)    # Solution de la plus grande selon la formule quadratique
 
-def print_solutions_of_parabola(a: int | float, b: int | float, c: int | float) -> None:
-    """
-    Cette fonction imprime à la console toutes les solutions réelles d'une équation du second degré de la forme ax²+bx+c où a≠0.
-    Args:
-        a (int ou float): coefficient associé à x puissance 2
-        b (int ou float): coefficient associé à x puissance 1
-        c (int ou float): coefficient associé à x puissance 0
-    """
+    if discriminant == 0: # Cas avec 1 solution
+        print(f"La solution à l'équation {a}x²+{b}x+{c} est {first_solution}.")
+        continue
 
-    solutions = get_solutions_of_parabola(a, b, c)
+    second_solution = (-b - sqrt(discriminant)) / (2 * a)   # Solution de la plus petite selon la formule quadratique
 
-    if len(solutions) == 0:
-        print(f"Il n'existe aucune solution à l'équation {a}x²+{b}x+{c}")
-        return
-    if len(solutions) == 1:
-        print(f"La solution à l'équation {a}x²+{b}x+{c} est {solutions[0]}")
-        return
-    
-    print(f"Les solutions à l'équation {a}x²+{b}x+{c} sont {solutions[0]} et {solutions[1]}")
-
-if __name__ == '__main__':
-    print_solutions_of_parabola(2, 5, 2)
-    print_solutions_of_parabola(1, 2, 1)
-    print_solutions_of_parabola(2, 2, 2)
+    print(f"Les solutions à l'équation {a}x²+{b}x+{c} sont {first_solution} et {second_solution}.")
